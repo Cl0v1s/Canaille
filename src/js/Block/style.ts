a0c3d5292b0521ac3d386e644f56ccd030a1722d
Clovis <clovis@bdx.town>
2024-01-12 09:08:41 +0100

    feat: add Block (#16)

---
+++ src/js/Block/style.ts
@@ -0,0 +1,29 @@
+import React from 'react';
+import { merge } from './../merge';
+
+function stateCSS(state): React.CSSProperties {
+    switch(state) {
+        default:
+        case "default": {
+            return {
+
+            }
+        }
+        case "hover": {
+            return {
+                "--background-color": "var(--additional-primary)",
+                "--box-shadow": "var(--dp-25)",
+            } as React.CSSProperties
+        }
+    }
+}
+
+export const block = (state) => merge({
+    "--border": "2px solid var(--grey-100)",
+    "--padding": "var(--spacing-3)",
+    "--background-color": "var(--white)",
+    "--box-shadow": "unset",
+    "--radius": "var(--rounded-100)",
+
+    "&:hover": stateCSS("hover"),
+} as React.CSSProperties, stateCSS(state));
